c4a43fee5b66c3c8c1d6e430f71c354ce58dac2d
yuchuan <yuchuan@gmail.com>
2019-08-02 11:06:47 -0400

    adding guessword test

---
+++ src/GuessedWords.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const GuessedWords = (props) => (
+    <div>
+
+    </div>
+);
+
+GuessedWords.propTypes = {
+    
+};
+
+export default GuessedWords;
---
+++ src/GuessedWords.test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { shallow } from "enzyme";
+import { findByTestAttr, checkProps } from "../test/testUtils";
+import GuessedWords from "./GuessedWords";
+
+const defaultProps = [
+  {
+    guessedWord: "xxx"
+  }
+];
